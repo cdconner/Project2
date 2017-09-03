0af21deeff04d53941a21dfdfee4b4d1ed81cdba
Chris Conner <chris.conner@gmail.com>
2017-09-03 16:35:55 -0500

    First Commit
    Setting up the game view controller and creating the three flags that
    will be exposed.
    Also added the logic to randomize the flags and to randomly select
    which of the options shown is the “correct answer.”

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -6,13 +6,60 @@
 //  Copyright © 2017 Chris Conner. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func askQuestion() {
+        
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
     }
 
     override func didReceiveMemoryWarning() {
